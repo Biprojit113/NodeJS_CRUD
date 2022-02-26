d63b0bae38ae2b142dca9864cd7ac49b0d373b16
Biprojit113 <soikotdey190@gmail.com>
2022-02-27 01:39:04 +0600

    Add Singin

--- API/Routes/users.js
+++ API/Routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const Users = require('../models/Users');     // importing Users Schema
 
 // Signup is also a process of post 
@@ -28,7 +29,8 @@ router.post('/signup',(req,res,next)=>{
             } 
             else{
                const information = {                                                  // printing the informations as object 
-                   _id : mongoose.Types.ObjectId(),                 
+                   _id : mongoose.Types.ObjectId(),
+                   name : req.body.name,                 
                    email: req.body.email,
                    password : hash
                }
@@ -43,9 +45,57 @@ router.post('/signup',(req,res,next)=>{
    })
    .catch(err=>res.status(500).json(err))
 
-   
-    
 })
+
+
+   // signin
+
+   router.post('/signin',(req,res,next)=>{
+    Users.findOne({email:req.body.email})                                           // finding the requested email by comparing with the database existed email
+    .exec()
+    .then(users=>
+     {
+         if(!users)                                                          // if there is no such email then close the process
+         {
+             res.status(500).json({
+                 message : "Authentication Failed"
+             })
+         }
+         else{
+             bcrypt.compare(req.body.password,users.password,(err,result)=>{                     // if there is an email exits then next step is to compare the password with req. password    
+                 if(err){ 
+                     console.log(err);                                                          // if it found false then quit the process
+                     res.status(500).json({
+                         message: "Password Wrong!!!!!"
+                     })
+                 }
+                 if(result)
+                 {
+                     // for taking process.env.JWT_KEY as a string we use `${process.env.JWT_KEY}`
+                    const token = jwt.sign({id: users._id,name:users.name,email:users.email},`${process.env.JWT_KEY}`,{           //passing the result with jWT token here's how to assign it process.env.JWT... is like process.env.MON_PASS
+                                                                                             // as a pass we can use any part of user here we used id,name,email
+                        expiresIn : 3600
+                    });
+                    
+                    res.status(200).json({
+                        message : "Authentication Successfull!!!",
+                        token : token
+                    })
+                 }
+                 else {
+                     res.status(500).json({
+                         message : "Try Again!!"
+                     })
+                 }
+                 
+                 
+             })
+         }
+     })
+     .catch(err=>res.status(500).json(err))
+})
+
+ 
 
 
 module.exports = router;
--- API/models/Users.js
+++ API/models/Users.js
@@ -7,7 +7,8 @@ const mongoose =  require('mongoose');
 
 const userSchema = mongoose.Schema({
 
-    _id : mongoose.Schema.Types.ObjectId,              //a unique id
+    _id : mongoose.Schema.Types.ObjectId,          // a unique id
+     name: String,          
     email: String,                                       // email and password for users
     password: String
 
